8e3f6635458f4972693aeb60b6b59d07cc1bf2b6
sinan altundağ <peykani@gmail.com>
2022-11-04 23:42:53 +0300

    button controls added

--- src/components/isometric.js
+++ src/components/isometric.js
@@ -6,7 +6,6 @@ import { GridEngine } from "grid-engine";
 // var game = new Phaser.Game(config);
 
 export default function App() {
-  const [speed, setSpeed] = useState(2);
 
   function createPlayerAnimation(name, startFrame, endFrame) {
     this.anims.create({
@@ -65,9 +64,9 @@ export default function App() {
         );
 
          this.game.events.on("myEvent", function (data) {
-          console.log(data);
-        });
-
+           console.log(data);
+          });
+          
         createPlayerAnimation.call(this, "up-right", 26, 29);
         createPlayerAnimation.call(this, "down-right", 36, 39);
         createPlayerAnimation.call(this, "down-left", 6, 9);
@@ -81,14 +80,10 @@ export default function App() {
               startPosition: { x: 0, y: 0 },
               offsetY: -9,
               walkingAnimationEnabled: false,
-              speed: speed,
+              speed: 2,
             },
           ],
         };
-        console.log(speed);
-        this.game.events.on("speed", () => {
-          console.log(speed);
-        });
         this.gridEngine.create(cloudCityTilemap, gridEngineConfig);
         this.gridEngine.movementStarted().subscribe(({ direction }) => {
           playerSprite.anims.play(direction);
@@ -115,6 +110,18 @@ export default function App() {
         } else if (cursors.down.isDown) {
           this.gridEngine.move("player", "down-left");
         }
+        const _this = this;
+        this.game.events.on("myEvent2", function (data) {
+          if (data==="down-left") {
+            _this.gridEngine.move("player", "down-left");
+          } else if (data==="down-right") {
+            _this.gridEngine.move("player", "down-right");
+          } else if (data==="up-left") {
+            _this.gridEngine.move("player", "up-left");
+          } else if (data==="up-right") {
+            _this.gridEngine.move("player", "up-right");
+          }
+         });
       },
     },
     plugins: {
@@ -144,7 +151,6 @@ export default function App() {
     }
   }, [initialize]);
   console.log(currentGame);
-  currentGame?.events.emit("myEvent", "hello");
   
   // Call `setInitialize` when you want to initialize your game! :)
   const destroy = async () => {
@@ -158,14 +164,19 @@ export default function App() {
   return (
     <>
       <IonPhaser game={game} ref={gameRef} initialize={initialize} />
-      <button onClick={() => setSpeed(speed + 1)}>Speed Up</button>
-      <button onClick={() => setSpeed(speed - 1)}>Speed Down</button>
       <button onClick={() => setGame(config)}>Change Level Back</button>
       <button onClick={() => setInitialize(true)}>Initialize</button>
       <button onClick={destroy}>Destroy</button>
       <button onClick={() => currentGame?.events.emit("myEvent", "hello everybody")}>
         say hello
       </button>
+      <div>
+      <button onClick={() => currentGame?.events.emit("myEvent2", "up-left")} >Up Left</button>
+      <button onClick={() => currentGame?.events.emit("myEvent2", "up-right")} >Up Right</button>
+      <button onClick={() => currentGame?.events.emit("myEvent2", "down-left")} >Down Left</button>
+      <button onClick={() => currentGame?.events.emit("myEvent2", "down-right")}>Down Right</button>
+
+      </div>
     </>
   );
 }
