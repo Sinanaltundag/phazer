3c8fa7d528de858bb4166ddbbb4505b1bd2bb4a9
sinan altundağ <peykani@gmail.com>
2022-11-13 08:37:59 +0300

    create new map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 // import IonEx from './components/ionexample';
 // import PhaserSimple from './components/PhaserSimple';
-import Isometric from './components/isometric';
+// import Isometric from './components/isometric';
 // import IsometricClass from './components/IsometricClass';
 // import Simple from './components/Simple';
+// import PlayerFuncs from './components/PlayerFuncs';
+// import UpCamera from './components/UpCamera';
+import NewMap from './components/NewMap';
 
 function App() {
   // const [start, setStart] = useState(false);
   return (
     <div className="App">
       {/* <IsometricClass /> */}
-      <Isometric/>
+      {/* <Isometric/> */}
       {/* {start?<IonEx/>:<Simple />}
       <button onClick={()=>setStart(!start)}>Start</button> */}
       {/* <IonEx/> */}
       {/* <PhaserSimple/> */}
+      {/* <PlayerFuncs/> */}
+      {/* <UpCamera/> */}
+      <NewMap/>
     </div>
   );
 }
